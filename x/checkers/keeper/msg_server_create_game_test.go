1cadee9c129b1600154d5eb7896a07efba7cd8ce
ppoliani <ppoliani@gmail.com>
2022-09-27 21:38:49 +0300

    test: fix failing create game test

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,9 +1,14 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	keepertest "github.com/apocentre/checkers/testutil/keeper"
+	"github.com/apocentre/checkers/x/checkers"
+	"github.com/apocentre/checkers/x/checkers/keeper"
 	"github.com/apocentre/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,9 +18,15 @@ const (
 	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	// Initialize the keeper with the default genesis
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
 
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
+	msgServer, _, context := setupMsgServerCreateGame(t)
 
 	response, err := msgServer.CreateGame(context, &types.MsgCreateGame {
 		Creator: alice,
